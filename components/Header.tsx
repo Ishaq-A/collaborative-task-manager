dd9bc53be6b2cee545af0ff37b6f2e72bb3307f7
Aman Ishaq <aman-ishaq@hotmail.com>
2024-10-13 21:32:08 +0100

    updated logo and size of logo, minor header changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,11 +6,11 @@ import React from 'react'
 const Header = ({ children, className }: HeaderProps) => {
   return (
     <div className={cn("header", className)}>
-        <Link href='/' className='md:flex-1'>
+        <Link href='/' className='md:flex-1 mt-5'>
             <Image 
                 src="/assets/icons/logo.svg"
                 alt="Logo with name"
-                width={120}
+                width={185}
                 height={32}
                 className="hidden md:block"
             />
